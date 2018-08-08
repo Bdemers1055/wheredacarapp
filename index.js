2b74496eb5097dec97cf7103250ed596120ab720
bdemers1055 <bdemers1055@gmail.com>
2018-08-08 19:16:18 -0400

    replaced port with 2004

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ dotenv.config();
 mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true});
 
 // setup our port
-const port = process.env.PORT || 2008;
+const port = process.env.PORT || 2004;
 
 // kick it off
 server.listen(port, () => {

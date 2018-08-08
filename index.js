f210f7f73ac27cab1a9d0ad68493e628878804c9
bdemers1055 <bdemers1055@gmail.com>
2018-08-08 19:00:33 -0400

    setup server connection with mongoose

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require ('express');
+const server = express();
+const dotenv = require ('dotenv');
+const mongoose = require ('mongoose');
+const bodyParser = require ('body-parser');
+const morgan = require ('morgan');
+const helmet = require ('helmet');
+
+// setup env variables
+dotenv.config();
+
+//connect to database
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true});
+
+// setup our port
+const port = process.env.PORT || 2008;
+
+// kick it off
+server.listen(port, () => {
+    console.log(`now listnening at port 2004`);
+});

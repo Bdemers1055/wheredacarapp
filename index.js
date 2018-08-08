dc020796a53ba89d91d033e5d4ad06c70fd5c241
bdemers1055 <bdemers1055@gmail.com>
2018-08-08 19:33:41 -0400

    setup initial user model, setup routes for getting all users, getting one user by id

--- index.js
+++ index.js
@@ -15,7 +15,51 @@ mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true});
 // setup our port
 const port = process.env.PORT || 2004;
 
+//models
+
+const User = mongoose.model('User', { name: String });
+
+//routers 
+
+//get all users
+server.get('/users', async (req, res, next) => {
+    try {
+        const users = await User.find();
+        res.status(200).json({
+            users: users
+        });
+    } 
+    catch(err) {
+        next(err);
+    }
+});
+
+
+// get user by id
+server.get('/users/:id', async (req, res, next) => {
+    const { id } = req.params; 
+    try {
+        const users = await User.find({ _id: id });
+        res.status(200).json({
+            users: users
+        });
+    }
+    catch(err) {
+        console.log('could not get user')
+        // next(err);
+    }
+});
+
+
+
+//power-ups, middlewares
+server.use(helmet());
+server.use(morgan("combined")); //status logging
+server.use(bodyParser.json()); //accept json data
+server.use(bodyParser.urlencoded({ extended: true })); //accept html form data 
+
 // kick it off
 server.listen(port, () => {
-    console.log(`now listnening at port 2004`);
+    console.log(`now listening at port 2004`);
 });
+

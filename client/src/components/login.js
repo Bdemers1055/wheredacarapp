f98216f3e2e866a0e24eb318b8bfe86691efd9b1
bdemers1055 <bdemers1055@gmail.com>
2018-08-09 19:57:46 -0400

    created login router

---
+++ client/src/components/login.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import '../App.css';
+
+class Login extends Component {
+    render() {
+      return (
+        <div className="loginInputGroup">
+                <input className="email" 
+                    type="email"
+                    placeholder="first.last@email.com" />
+                <input className="password" 
+                    type="text"
+                    placeholder="password" />
+                <button type="button" className="loginBtn">LOGIN</button>
+        </div>
+      );
+    }
+};
+
+export default Login;

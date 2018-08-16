603c3591d6640725fc948d5c19d13b7199d2790b
bdemers1055 <bdemers1055@gmail.com>
2018-08-15 20:06:38 -0400

    initial signup test, error on submit

--- client/src/pages/signup.js
+++ client/src/pages/signup.js
@@ -1,19 +1,43 @@
 import React, { Component } from 'react';
+import auth from '../services/auth';
 import '../App.css';
 
 class Signup extends Component {
+        state = {
+            error: null
+        }
+        async signup(e){
+            e.preventDefault();
+            const form = e.target;
+            const inputs = Array.from(form.elements)
+                                .filter(element => element.tagName === 'INPUT');
+            const [ email, password ] = inputs.map(input => input.value);
+            try {
+                await auth.signup(email, password);
+                this.props.history.push({
+                    pathname: '/login'
+                });
+                
+            } catch (error) {
+                this.setState({
+                    error: 'try again'
+            });
+            form.reset();
+        }
+    }
     render() {
       return (
-        <div className="SignupInputGroup">
+        <form className="SignupInputGroup" onSubmit={this.signup.bind(this)}>
+        {this.state.error ? <p>{this.state.error}</p> : null }
                 <input className="email" 
                     type="email"
                     placeholder="first.last@email.com" />
                 <input className="password" 
                     type="text"
                     placeholder="password" />
-                <button type="button" className="loginBtn">SIGNUP</button>
+                <button type="submit" className="loginBtn">SIGNUP</button>
                 <p>Already have an account?<a href="/login"> Login</a></p>
-        </div>
+        </form>
       );
     }
 };
